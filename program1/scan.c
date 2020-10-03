d27acec996323ac5778594e25664e27617807c78
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-10-03 23:09:39 +0900

    :sparkles: コンパイルに必要な関数をscan.cに定義した

---
+++ program1/scan.c
@@ -0,0 +1,16 @@
+#include "token-list.h"
+
+int init_scan(char *filename) {
+    // comment
+    return 0;
+}
+
+int scan(void) {
+    return 0;
+}
+int get_linenum(void) {
+    return 0;
+}
+void end_scan(void) {
+    return;
+}
