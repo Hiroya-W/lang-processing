a33b7a85a3c80f4aa0f038fb8ed4815caf3c1ef6
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-10-05 16:14:16 +0900

    :rotating_light: テストを追加

--- program1/test.c
+++ program1/test.c
@@ -10,6 +10,7 @@
 
 void scan_test_001(void);
 void scan_test_002(void);
+void scan_test_003(void);
 
 #undef main
 int main() {
@@ -19,6 +20,8 @@ int main() {
 
     suite = CU_add_suite("Scan Test ", NULL, NULL);
     CU_add_test(suite, "test_001", scan_test_001);
+    CU_add_test(suite, "test_002", scan_test_002);
+    CU_add_test(suite, "test_003", scan_test_003);
     CU_console_run_tests();
     CU_cleanup_registry();
     return 0;
@@ -27,9 +30,22 @@ int main() {
 void scan_test_001(void) {
     CU_ASSERT(_isblank(' ') == 1);
     CU_ASSERT(_isblank('\t') == 1);
+    CU_ASSERT(_isblank('\n') == 0);
+    CU_ASSERT(_isblank('a') == 0);
 }
 
 void scan_test_002(void) {
-    CU_ASSERT(get_keyword_token_code("and") == TAND);
-    CU_ASSERT(get_keyword_token_code("array") == TARRAY);
+    int i;
+    for (i = 0; i < KEYWORDSIZE; i++) {
+        CU_ASSERT(get_keyword_token_code(key[i].keyword) == key[i].keytoken);
+    }
+    CU_ASSERT(get_keyword_token_code("ABCDE") == TNAME);
+    CU_ASSERT(get_keyword_token_code("AB12") == TNAME);
+}
+
+void scan_test_003(void) {
+    init_scan("samples/sample11.mpl");
+    CU_ASSERT(scan() == TPROGRAM);
+    CU_ASSERT(scan() == TNAME);
+    end_scan();
 }
