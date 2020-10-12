fc370d7c7f1cb27a3bd38d8dabb7af99797c92a5
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-10-12 16:57:11 +0900

    :books: token-list.hにドキュメントを書いた #5

--- program1/token-list.h
+++ program1/token-list.h
@@ -6,70 +6,127 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*! maximum length of a string */
 #define MAXSTRSIZE 1024
 
-/* Token */
-#define TNAME 1       /* Name : Alphabet { Alphabet | Digit } */
-#define TPROGRAM 2    /* program : Keyword */
-#define TVAR 3        /* var : Keyword */
-#define TARRAY 4      /* array : Keyword */
-#define TOF 5         /* of : Keyword */
-#define TBEGIN 6      /* begin : Keyword */
-#define TEND 7        /* end : Keyword */
-#define TIF 8         /* if : Keyword */
-#define TTHEN 9       /* then : Keyword */
-#define TELSE 10      /* else : Keyword */
-#define TPROCEDURE 11 /* procedure : Keyword */
-#define TRETURN 12    /* return : Keyword */
-#define TCALL 13      /* call : Keyword */
-#define TWHILE 14     /* while : Keyword */
-#define TDO 15        /* do : Keyword */
-#define TNOT 16       /* not : Keyword */
-#define TOR 17        /* or : Keyword */
-#define TDIV 18       /* div : Keyword */
-#define TAND 19       /* and : Keyword */
-#define TCHAR 20      /* char : Keyword */
-#define TINTEGER 21   /* integer : Keyword */
-#define TBOOLEAN 22   /* boolean : Keyword */
-#define TREADLN 23    /* readln : Keyword */
-#define TWRITELN 24   /* writeln : Keyword */
-#define TTRUE 25      /* true : Keyword */
-#define TFALSE 26     /* false : Keyword */
-#define TNUMBER 27    /* unsigned integer */
-#define TSTRING 28    /* String */
-#define TPLUS 29      /* + : symbol */
-#define TMINUS 30     /* - : symbol */
-#define TSTAR 31      /* * : symbol */
-#define TEQUAL 32     /* = : symbol */
-#define TNOTEQ 33     /* <> : symbol */
-#define TLE 34        /* < : symbol */
-#define TLEEQ 35      /* <= : symbol */
-#define TGR 36        /* > : symbol */
-#define TGREQ 37      /* >= : symbol */
-#define TLPAREN 38    /* ( : symbol */
-#define TRPAREN 39    /* ) : symbol */
-#define TLSQPAREN 40  /* [ : symbol */
-#define TRSQPAREN 41  /* ] : symbol */
-#define TASSIGN 42    /* := : symbol */
-#define TDOT 43       /* . : symbol */
-#define TCOMMA 44     /* , : symbol */
-#define TCOLON 45     /* : : symbol */
-#define TSEMI 46      /* ; : symbol */
-#define TREAD 47      /* read : Keyword */
-#define TWRITE 48     /* write : Keyword */
-#define TBREAK 49     /* break : Keyword */
+/*! @name definition of token code */
+/* @{ */
+/*! Name : Alphabet { Alphabet | Digit } */
+#define TNAME 1
+/*! program : Keyword */
+#define TPROGRAM 2
+/*! var : Keyword */
+#define TVAR 3
+/*! array : Keyword */
+#define TARRAY 4
+/*! of : Keyword */
+#define TOF 5
+/*! begin : Keyword */
+#define TBEGIN 6
+/*! end : Keyword */
+#define TEND 7
+/*! if : Keyword */
+#define TIF 8
+/*! then : Keyword */
+#define TTHEN 9
+/*! else : Keyword */
+#define TELSE 10
+/*! procedure : Keyword */
+#define TPROCEDURE 11
+/*! return : Keyword */
+#define TRETURN 12
+/*! call : Keyword */
+#define TCALL 13
+/*! while : Keyword */
+#define TWHILE 14
+/*! do : Keyword */
+#define TDO 15
+/*! not : Keyword */
+#define TNOT 16
+/*! or : Keyword */
+#define TOR 17
+/*! div : Keyword */
+#define TDIV 18
+/*! and : Keyword */
+#define TAND 19
+/*! char : Keyword */
+#define TCHAR 20
+/*! integer : Keyword */
+#define TINTEGER 21
+/*! boolean : Keyword */
+#define TBOOLEAN 22
+/*! readln : Keyword */
+#define TREADLN 23
+/*! writeln : Keyword */
+#define TWRITELN 24
+/*! true : Keyword */
+#define TTRUE 25
+/*! false : Keyword */
+#define TFALSE 26
+/*! unsigned integer */
+#define TNUMBER 27
+/*! String */
+#define TSTRING 28
+/*! + : symbol */
+#define TPLUS 29
+/*! - : symbol */
+#define TMINUS 30
+/*! * : symbol */
+#define TSTAR 31
+/*! = : symbol */
+#define TEQUAL 32
+/*! <> : symbol */
+#define TNOTEQ 33
+/*! < : symbol */
+#define TLE 34
+/*! <= : symbol */
+#define TLEEQ 35
+/*! > : symbol */
+#define TGR 36
+/*! >= : symbol */
+#define TGREQ 37
+/*! ( : symbol */
+#define TLPAREN 38
+/*! ) : symbol */
+#define TRPAREN 39
+/*! [ : symbol */
+#define TLSQPAREN 40
+/*! ] : symbol */
+#define TRSQPAREN 41
+/*! := : symbol */
+#define TASSIGN 42
+/*! . : symbol */
+#define TDOT 43
+/*! , : symbol */
+#define TCOMMA 44
+/*! : : symbol */
+#define TCOLON 45
+/*! ; : symbol */
+#define TSEMI 46
+/*! read : Keyword */
+#define TREAD 47
+/*! write : Keyword */
+#define TWRITE 48
+/*! break : Keyword */
+#define TBREAK 49
+/* @} */
 
+/*! number of tokens */
 #define NUMOFTOKEN 49
 
+/*! maximum number of an unsigned integer */
 #define MAX_NUM_ATTR 32767
 
 /* token-list.c */
-
+/*! number of keywords */
 #define KEYWORDSIZE 28
 
+/*!
+ * @brief A pair of token codes for a keyword
+ */
 extern struct KEY {
-    char *keyword;
-    int keytoken;
+    char *keyword; /*! keyword strings */
+    int keytoken;  /*! token codes for keywords */
 } key[KEYWORDSIZE];
 
 extern void error(char *mes);
