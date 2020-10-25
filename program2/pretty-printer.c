8c473954847b36978d942e6c586fb5c9f6c21fa7
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-10-25 21:41:54 +0900

    :sparkles: 現状の整形結果を出力するようにした

--- program2/pretty-printer.c
+++ program2/pretty-printer.c
@@ -1,10 +1,24 @@
 #include "pretty-printer.h"
 
+#include <stdio.h>
+
+/*! String of each token */
+char *tokenstr[NUMOFTOKEN + 1] = {
+    "", "NAME", "program", "var", "array", "of", "begin",
+    "end", "if", "then", "else", "procedure", "return", "call",
+    "while", "do", "not", "or", "div", "and", "char",
+    "integer", "boolean", "readln", "writeln", "true", "false", "NUMBER",
+    "STRING", "+", "-", "*", "=", "<>", "<",
+    "<=", ">", ">=", "(", ")", "[", "]",
+    ":=", ".", ",", ":", ";", "read", "write",
+    "break"};
+
 static int parse_block(void);
 static int parse_variable_declaration(void);
 static int parse_variable_names(void);
 static int parse_compound_statement(void);
 static int parse_type(void);
+static int parse_subprogram_declaration(void);
 
 /*!
  * @brief Parsing a program
@@ -14,21 +28,31 @@ int parse_program(void) {
     if (token != TPROGRAM) {
         return error("Keyword 'program' is not found.");
     }
+    fprintf(stdout, "%s ", tokenstr[token]);
+
     token = scan();
     if (token != TNAME) {
         return error("Program name is not found.");
     }
+    fprintf(stdout, "%s", string_attr);
+
     token = scan();
     if (token != TSEMI) {
         return error("Semicolon is not found.");
     }
+    fprintf(stdout, "%s", tokenstr[token]);
+    fprintf(stdout, "\n");
+
     token = scan();
     if (parse_block() == ERROR) {
         return ERROR;
     }
+
     if (token != TDOT) {
         return error("Period is not found at the end of program.");
     }
+    fprintf(stdout, "%s ", tokenstr[token]);
+
     token = scan();
     return NORMAL;
 }
@@ -44,6 +68,9 @@ static int parse_block(void) {
                 return ERROR;
             }
         } else {
+            if (parse_subprogram_declaration() == ERROR) {
+                return ERROR;
+            }
         }
     }
     if (parse_compound_statement() == ERROR) {
@@ -61,6 +88,7 @@ static int parse_variable_declaration(void) {
     if (token != TVAR) {
         return error("Keyword 'var' is not found.");
     }
+    fprintf(stdout, "%s ", tokenstr[token]);
 
     token = scan();
     while (token == TNAME) {
@@ -73,10 +101,12 @@ static int parse_variable_declaration(void) {
         if (parse_variable_names() == ERROR) {
             return ERROR;
         }
-        token = scan();
+
         if (token != TCOLON) {
             return error("Colon is not found.");
         }
+        fprintf(stdout, "%s ", tokenstr[token]);
+
         if (parse_type() == ERROR) {
             return ERROR;
         }
@@ -92,18 +122,30 @@ static int parse_variable_names(void) {
     if (token != TNAME) {
         return error("Name is not found.");
     }
+    fprintf(stdout, "%s", string_attr);
+
     token = scan();
     while (token == TCOMMA) {
+        fprintf(stdout, "%s ", tokenstr[token]);
+
         token = scan();
         if (token != TNAME) {
             return error("Name is not found.");
         }
+        fprintf(stdout, "%s ", string_attr);
+        token = scan();
     }
     return NORMAL;
 }
+
 static int parse_compound_statement(void) {
-    return ERROR;
+    return error("Unimplemented");
 }
+
 static int parse_type(void) {
-    return ERROR;
+    return error("Unimplemented");
+}
+
+static int parse_subprogram_declaration(void) {
+    return error("Unimplemented");
 }
