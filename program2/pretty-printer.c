80b78ddb22b80db01106587bb1c079d9628c9f61
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-10-25 18:32:55 +0900

    :construction: ブロック部の構文解析を書いた

--- program2/pretty-printer.c
+++ program2/pretty-printer.c
@@ -1,34 +1,56 @@
 #include "pretty-printer.h"
 
 int block();
+int hukugoubun();
 
+/*!
+ * @brief Parsing a program
+ * @return int Returns 0 on success and 1 on failure.
+ */
 int parse_program(void) {
     if (token != TPROGRAM) {
-        error("Keyword 'program' is not found");
-        return EXIT_FAILURE;
+        error("Keyword 'program' is not found.");
+        return ERROR;
     }
     token = scan();
     if (token != TNAME) {
-        error("Program name is not found");
-        return EXIT_FAILURE;
+        error("Program name is not found.");
+        return ERROR;
     }
     token = scan();
     if (token != TSEMI) {
-        error("Semicolon is not found");
-        return EXIT_FAILURE;
+        error("Semicolon is not found.");
+        return ERROR;
     }
     token = scan();
-    if (block() == EXIT_FAILURE) {
-        return EXIT_FAILURE;
+    if (block() == ERROR) {
+        return ERROR;
     }
     if (token != TDOT) {
-        error("Period is not found at the end of program");
-        return EXIT_FAILURE;
+        error("Period is not found at the end of program.");
+        return ERROR;
     }
     token = scan();
-    return EXIT_SUCCESS;
+    return NORMAL;
 }
 
-int block() {
-    return EXIT_SUCCESS;
+/*!
+ * @brief Parsing a block
+ * @return int Returns 0 on success and 1 on failure.
+ */
+int block(void) {
+    /* 
+     * variable declaration : TVAR
+     * subprogram declaration : TPROCEDURE
+     * composite statement : TBEGIN
+     */
+    while (token == TVAR || token == TPROCEDURE) {
+        if (token == TVAR) {
+        } else {
+        }
+    }
+    if (hukugoubun() == ERROR) {
+        return ERROR;
+    }
+    return NORMAL;
 }
