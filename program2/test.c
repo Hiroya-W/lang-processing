d07309ef8fa6eff918e3d8985114fc8eb32cb84c
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-11-15 19:20:47 +0900

    :sparkles: テストを追加した

--- program2/test.c
+++ program2/test.c
@@ -12,7 +12,18 @@
 #include "pretty-printer.c"
 #include "scan.c"
 
+void parse(void);
+void check(char *output, char *answer);
 void integration_test_sample29p(void);
+void integration_test_sample021(void);
+void integration_test_sample022(void);
+void integration_test_sample023(void);
+void integration_test_sample024(void);
+void integration_test_sample025(void);
+void integration_test_sample026(void);
+void integration_test_sample02a(void);
+void integration_test_parse_formal_parameters(void);
+void integration_test_parse_output_format(void);
 
 #undef main
 int main() {
@@ -22,6 +33,15 @@ int main() {
 
     suite = CU_add_suite("Integration Test", NULL, NULL);
     CU_add_test(suite, "integration_test_sample29p", integration_test_sample29p);
+    CU_add_test(suite, "integration_test_sample021", integration_test_sample021);
+    CU_add_test(suite, "integration_test_sample022", integration_test_sample022);
+    CU_add_test(suite, "integration_test_sample023", integration_test_sample023);
+    CU_add_test(suite, "integration_test_sample024", integration_test_sample024);
+    CU_add_test(suite, "integration_test_sample025", integration_test_sample025);
+    CU_add_test(suite, "integration_test_sample026", integration_test_sample026);
+    CU_add_test(suite, "integration_test_sample02a", integration_test_sample02a);
+    CU_add_test(suite, "integration_test_parse_formal_parameters", integration_test_parse_formal_parameters);
+    CU_add_test(suite, "integration_test_parse_output_format", integration_test_parse_output_format);
 
     CU_basic_run_tests();
 
@@ -36,10 +56,130 @@ int main() {
     }
 }
 
+void integration_test_sample021(void) {
+    char *output = "./outputs/sample021.mpl";
+    char *answer = "./answers/sample021.mpl";
+    file_name = "./samples/sample021.mpl";
+
+    parse();
+    check(output, answer);
+
+    return;
+}
+
+void integration_test_sample022(void) {
+    char *output = "./outputs/sample022.mpl";
+    char *answer = "./answers/sample022.mpl";
+    file_name = "./samples/sample022.mpl";
+
+    parse();
+    check(output, answer);
+
+    return;
+}
+
+void integration_test_sample023(void) {
+    char *output = "./outputs/sample023.mpl";
+    char *answer = "./answers/sample023.mpl";
+    file_name = "./samples/sample023.mpl";
+
+    parse();
+    check(output, answer);
+
+    return;
+}
+
+void integration_test_sample024(void) {
+    char *output = "./outputs/sample024.mpl";
+    char *answer = "./answers/sample024.mpl";
+    file_name = "./samples/sample024.mpl";
+
+    parse();
+    check(output, answer);
+
+    return;
+}
+
+void integration_test_sample025(void) {
+    char *output = "./outputs/sample025.mpl";
+    char *answer = "./answers/sample025.mpl";
+    file_name = "./samples/sample025.mpl";
+
+    parse();
+    check(output, answer);
+
+    return;
+}
+
+void integration_test_sample026(void) {
+    char *output = "./outputs/sample026.mpl";
+    char *answer = "./answers/sample026.mpl";
+    file_name = "./samples/sample026.mpl";
+
+    parse();
+    check(output, answer);
+
+    return;
+}
+
+void integration_test_sample02a(void) {
+    char *output = "./outputs/sample02a.mpl";
+    char *answer = "./answers/sample02a.mpl";
+    file_name = "./samples/sample02a.mpl";
+
+    parse();
+    check(output, answer);
+
+    return;
+}
+
 void integration_test_sample29p(void) {
     char *output = "./outputs/sample29p.mpl";
     char *answer = "./answers/sample29p.mpl";
 
+    file_name = "./samples/sample29p.mpl";
+
+    parse();
+    check(output, answer);
+
+    return;
+}
+
+void integration_test_parse_formal_parameters(void) {
+    char *output = "./outputs/parse_formal_parameters.mpl";
+    char *answer = "./answers/parse_formal_parameters.mpl";
+
+    file_name = "./samples/parse_formal_parameters.mpl";
+
+    parse();
+    check(output, answer);
+
+    return;
+}
+
+void integration_test_parse_output_format(void) {
+    char *output = "./outputs/parse_output_format.mpl";
+    char *answer = "./answers/parse_output_format.mpl";
+
+    file_name = "./samples/parse_output_format.mpl";
+
+    parse();
+    check(output, answer);
+
+    return;
+}
+
+void parse(void) {
+    init_scan(file_name);
+    indent_level = 0;
+    token = scan();
+    parse_program();
+    end_scan();
+    fprintf(stdout, "\n");
+    fflush(stdout);
+}
+
+void check(char *output, char *answer) {
     FILE *fp_output;
     FILE *fp_answer;
 
@@ -73,6 +213,4 @@ void integration_test_sample29p(void) {
         memset(out_buf, '\0', sizeof(out_buf));
         memset(ans_buf, '\0', sizeof(ans_buf));
     }
-
-    return;
 }
