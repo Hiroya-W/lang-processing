e66abdffa3b7f345adb32dbf434495451b0c120e
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-11-30 20:33:19 +0900

    :sparkles: id-list.cを追加

---
+++ program3/id-list.c
@@ -0,0 +1,29 @@
+﻿#include "mppl_compiler.h"
+
+struct TYPE {
+    int ttype;           /* TPINT TPCHAR TPBOOL TPARRAY TPARRAYINT TPARRAYCHA RTPARRAYBOOL TPPROC */
+    int arraysize;       /* size of array, if TPARRAY */
+    struct TYPE *etp;    /* pointer to element type if TPARRAY */
+    struct TYPE *paratp; /* pointer to parameter's type list if ttype is TPPROC */
+};
+
+struct LINE {
+    int reflinenum;
+    struct LINE *nextlinep;
+};
+
+struct ID {
+    char *name;
+    char *procname; /* procedure name within which this name is defined */ /* NULL if global name */
+    struct TYPE *itp;
+    int ispara; /* 1:formal parameter, 0:else(variable) */
+    int deflinenum;
+    struct LINE *irefp;
+    struct ID *nextp;
+} * globalidroot, *localidroot; /* Pointers to root of global & local symbol tables */
+
+/* Initialise the table */
+void init_idtab() {
+    globalidroot = NULL;
+    localidroot = NULL;
+}
