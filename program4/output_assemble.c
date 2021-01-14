6ba7a6b47d884201ba27ba00e1802b9fb5eb202a
Hiroya-W <b8122508@edu.kit.ac.jp>
2021-01-14 18:02:01 +0900

    :+1: GR2の中身をSTするようにした

--- program4/output_assemble.c
+++ program4/output_assemble.c
@@ -157,6 +157,7 @@ void assemble_variable_reference(struct ID *referenced_variable) {
 void assemble_assign(void) {
     fprintf(out_fp, "\tPOP \tgr2\n");
     fprintf(out_fp, "\tPOP \tgr1\n");
+    fprintf(out_fp, "\tLD \tgr2, \t0, \tgr2\n");
     fprintf(out_fp, "\tST \tgr2, \t0, \tgr1\n");
     fprintf(out_fp, "\tPUSH \t0, \tgr1\n");
 }
