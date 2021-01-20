8359bf3a7a4a3c6eb838e561889329efe3880571
Hiroya-W <b8122508@edu.kit.ac.jp>
2021-01-20 10:54:12 +0900

    :bug: *.mplだけを読み込むようにした

--- program4/output.sh
+++ program4/output.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p outputs
-for filepath in `\find samples/ -maxdepth 2 -type f | sort`; do
+for filepath in `\find samples/ -maxdepth 2 -type f -name "*.mpl" | sort`; do
     filename="${filepath##*/}"
     ./test $filepath
 done
